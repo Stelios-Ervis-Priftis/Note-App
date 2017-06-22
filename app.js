4b60c503b39e425d2c61f01c907dbb6ba380751f
Ervis-Priftis <steliospriftis@gmail.com>
2017-06-22 02:40:25 +0100

    Require Module os.userInfo()

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 console.log('Starting App.');
 
+// fs Asynchronously append data to a file, creating the file if does not yet exist
 const fs = require('fs');
 
-fs.appendFile('greeting.txt', 'Hello World!');
+// os.userInfo method return information about the currently effective User
+const os = require('os');
+
+var user = os.userInfo();
+console.log(user);
+
+fs.appendFile('greeting.txt', `Hello ${user.username}!`);
