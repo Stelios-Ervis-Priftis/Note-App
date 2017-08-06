c13a43304e296d41ed0094089cac5f1735bdcb46
Ervis-Priftis <steliospriftis@gmail.com>
2017-08-06 12:16:53 +0100

    npm init for package.json

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 console.log('Starting App.');
 
-// fs Asynchronously append data to a file, creating the file if does not yet exist
-const fs = require('fs');
-
-// os.userInfo method return information about the currently effective User
-const os = require('os');
-
-// require my own file or function
-const notes = require('./notes.js');
+const fs = require('fs'); // fs Asynchronously append data to a file, creating the file if does not yet exist
+const os = require('os'); // os.userInfo method return information about the currently effective User
+const notes = require('./notes.js'); // require my own file or function
 
 console.log('Result:', notes.add(9, 20));
 
