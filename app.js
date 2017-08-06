b37f8ffa82b5d924a63ab590aa73386a82f8fa12
Ervis-Priftis <steliospriftis@gmail.com>
2017-08-06 12:58:48 +0100

    Getting Input from User

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 console.log('Starting App.');
 
 const fs = require('fs'); // fs Asynchronously append data to a file, creating the file if does not yet exist
-const os = require('os'); // os.userInfo method return information about the currently effective User
+// const os = require('os'); // os.userInfo method return information about the currently effective User
 const _ = require('lodash');
+
 const notes = require('./notes.js'); // require my own file or function
 
+var command = process.argv[2];
+console.log('Command', command);
+console.log(process.argv);
 
-
-var filteredArray = _.uniq(['Ervis', 1, 'Ervis', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-
-
-// console.log(_.isString(true));
-// console.log(_.isString('Ervis'));
-
-// console.log('Result:', notes.add(9, 20));
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greeting.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command not recognized');
+}
