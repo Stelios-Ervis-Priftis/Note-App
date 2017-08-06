aec5931c2f6fc3fa061faa1b88f9317acd70e231
Ervis-Priftis <steliospriftis@gmail.com>
2017-08-06 12:30:36 +0100

    npm lodash and required

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ console.log('Starting App.');
 
 const fs = require('fs'); // fs Asynchronously append data to a file, creating the file if does not yet exist
 const os = require('os'); // os.userInfo method return information about the currently effective User
+const _ = require('lodash');
 const notes = require('./notes.js'); // require my own file or function
 
-console.log('Result:', notes.add(9, 20));
+
+
+var filteredArray = _.uniq(['Ervis', 1, 'Ervis', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+
+
+// console.log(_.isString(true));
+// console.log(_.isString('Ervis'));
+
+// console.log('Result:', notes.add(9, 20));
 
 // var user = os.userInfo();
 //
